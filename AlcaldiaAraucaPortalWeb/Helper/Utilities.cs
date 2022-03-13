cc87702a4be9bafaed5f719332750ec77df06206
jose manuel bello romero <jomabero300@gmail.com>
2022-03-13 09:27:48 -0500

    Modificacion deespacios en utilidad

--- AlcaldiaAraucaPortalWeb/Helper/Utilities.cs
+++ AlcaldiaAraucaPortalWeb/Helper/Utilities.cs
@@ -10,7 +10,9 @@ namespace AlcaldiaAraucaPortalWeb.Helper
         public static string StartCharacterToUpper(string str)
         {
             string x = str.Substring(0, 1);
+
             x = x.ToUpper();
+
             str = x + str.Substring(1, str.Length - 1);
 
             return str;
